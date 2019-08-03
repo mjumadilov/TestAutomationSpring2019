2c989093f3d22811f547b403018c2ddd7b3cff9b
mjumadilov <maratckg@gmail.com>
2019-08-02 19:26:06 -0500

    just updateed

--- src/test/java/com/cybertek/MyPractice/quiz.java
+++ src/test/java/com/cybertek/MyPractice/quiz.java
@@ -9,8 +9,6 @@ public class quiz {
         byte b = 10;
         switch(b)
         {
-            case 10: System.out.print(10);
             case 100: System.out.print(100);
-            case (byte) 1000: System.out.print(1000);
         }
     }}
