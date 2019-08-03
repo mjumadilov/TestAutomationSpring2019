98280769eb8e7c078955439b066c8ad24a25200d
mjumadilov <51764952+mjumadilov@users.noreply.github.com>
2019-08-02 19:31:56 -0500

    Update quiz.java
    Test update

--- src/test/java/com/cybertek/MyPractice/quiz.java
+++ src/test/java/com/cybertek/MyPractice/quiz.java
@@ -10,5 +10,8 @@ public class quiz {
         switch(b)
         {
             case 100: System.out.print(100);
+                
+                
+            case 200: System.out.print(200);    
         }
     }}
