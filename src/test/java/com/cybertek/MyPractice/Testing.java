5e281aea23cac0fb7341214ae63872d9656cf9bb
mjumadilov <maratckg@gmail.com>
2019-08-21 18:39:34 -0500

    just updated

--- src/test/java/com/cybertek/MyPractice/Testing.java
+++ src/test/java/com/cybertek/MyPractice/Testing.java
@@ -1,4 +1,16 @@
 package com.cybertek.MyPractice;
 
+import com.cybertek.utilities.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+
 public class Testing {
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.get("http://google.com");
+        driver.manage().window().maximize();
+
+    }
 }
